135d0db9991a06b1cee11e929e981927dd880cbd
jose maria <josemb125@gmail.com>
2020-09-21 11:38:01 -0500

    deploy 0.3

---
+++ docs/precache-manifest.78eb8fd7409c8193216344f1a430930e.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ef0d9051278e3a72cff3bd855158d153",
+    "url": "./index.html"
+  },
+  {
+    "revision": "27160e5140d6b7bd2d44",
+    "url": "./static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "a4a219da4d0bcee4884a",
+    "url": "./static/css/main.0f1c5e45.chunk.css"
+  },
+  {
+    "revision": "27160e5140d6b7bd2d44",
+    "url": "./static/js/2.c35d06a7.chunk.js"
+  },
+  {
+    "revision": "ca3cfcda77dc9f4f71fda9fdfcf14fb0",
+    "url": "./static/js/2.c35d06a7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a4a219da4d0bcee4884a",
+    "url": "./static/js/main.3afebcf8.chunk.js"
+  },
+  {
+    "revision": "9d61b00b128cb3d30da4",
+    "url": "./static/js/runtime-main.84f4350d.js"
+  },
+  {
+    "revision": "396f90771b84b80229cd995204d6acde",
+    "url": "./static/media/banner.396f9077.jpg"
+  }
+]);
